4972fd8000fbbfa049c22ac2c658cf0427e83661
cc22ji <133473522+cc22ji@users.noreply.github.com>
2024-01-07 18:43:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8000;
 //use cors
 // app.use(cors())
 const corsOptions = {
-    origin: 'https://movieapp-backend-x3ry.onrender.com/',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
